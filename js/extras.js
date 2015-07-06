fe7b68693569c6ad629e39cdcfa2b1a177711709
Yuan <y.yuan@berkeley.edu>
2015-07-06 16:02:55 -0700

    scroll offset

--- js/extras.js
+++ js/extras.js
@@ -1,3 +1,17 @@
 function faqToggler(divId) {
     $("#" + divId).toggleClass("hidden");
 }
+
+function offsetAnchor() {
+    if(location.hash.length !== 0) {
+        window.scrollTo(window.scrollX, window.scrollY -50);
+    }
+}
+
+$(window).on("hashchange", function () {
+    offsetAnchor();
+});
+
+window.setTimeout(function() {
+    offsetAnchor();
+}, 1);
